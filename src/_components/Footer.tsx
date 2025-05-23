928fe355330bc9a6055e019a7fedd5eed4fa567c
masaya-ikeno-ss <masaya.ikeno@smart-group.co.jp>
2025-05-23 11:19:16 +0900

    ヘッダーとフッターの表示

---
+++ src/_components/Footer.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const Footer = () => {
+  return (
+    <footer style={{ padding: "16px 0", textAlign: "center", background: "#4d8cf5", color: "#fff" }}>
+      <small>&copy; {new Date().getFullYear()} POND POND</small>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/_components/Header.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const Header = () => {
+  return (
+    <header style={{ padding: "16px 0", textAlign: "center", background: "#4d8cf5", color: "#fff" }}>
+      <h1>ショッピングアプリ</h1>
+    </header>
+  )
+}
+
+export default Header
