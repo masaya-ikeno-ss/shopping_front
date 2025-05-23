9d4a8e93638bbe6fc5b9cb2a4b744746d429ae9d
masaya-ikeno-ss <masaya.ikeno@smart-group.co.jp>
2025-05-23 14:01:13 +0900

    ハンバーガーメニューの実装

--- src/_components/header/Header.tsx
+++ src/_components/header/Header.tsx
@@ -1,10 +1,32 @@
 "use client"
+import Link from "next/link"
 import styles from "./Header.module.css"
+import { useState } from "react"
 
 const Header = () => {
+  const [open, setOpen] = useState(false)
+
   return (
     <header className={styles.header}>
-      <h1>ショッピングアプリ</h1>
+      <h1>SHOPPING</h1>
+      <div 
+        className={`${styles.spanContainer} ${open ? styles.open : ""}`} 
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <div 
+        className={`${styles.navContainer} ${open ? styles.active : ""}`}
+        onClick={() => setOpen(false)}
+      >
+        <Link href="/" className={styles.navLink}>Home</Link>
+        <Link href="/" className={styles.navLink}>Category</Link>
+        <Link href="/" className={styles.navLink}>Cart</Link>
+        <Link href="/" className={styles.navLink}>Order History</Link>
+      </div>
     </header>
   )
 }
